ac2145f610969aec300accece30ec77794e4e205
Aleksandr Muraviov <amur@elvees.com>
2014-12-24 11:31:52 +0300

    Reverting workaround for fixing problem with global std locale settings.

--- OnvifSDK/source/DeviceInfoStorage.cpp
+++ OnvifSDK/source/DeviceInfoStorage.cpp
@@ -83,7 +83,6 @@ DeviceInfoStorage::SetDeviceInfo( OnvifDevice::Type type,
     m_webservicePort = webservicePort;
 
     std::stringstream ss;
-    ss.imbue(std::locale("C"));
     ss << "http://" <<  m_deviceServiceXAddr << ":" << m_webservicePort;
     m_endpoint = ss.str();
     if( m_deviceServiceXAddr.empty() ) {
